30ccfc3e9de98307f48d5a4b1f379f282843ae32
翁某小小 <343577402@qq.com>
2019-02-23 00:06:26 +0800

    go 基础语法 数组

--- arrays.go
+++ arrays.go
@@ -2,11 +2,25 @@ package main
 
 import "fmt"
 
+func printArr(arr [5] int) {
+	arr[0] = 100
+	for i, v := range arr {
+		fmt.Println(i, v)
+	}
+}
+
+func printArr2(arr *[5]int) {
+	(*arr)[0] = 100
+	for i, v := range arr {
+		fmt.Println(i, v)
+	}
+}
+
 func main() {
 
 	var arr1 [5]int
 	arr2 := [3]int{1, 2, 3}
-	arr3 := [...]int{1, 2, 3, 4, 5, 6}
+	arr3 := [...]int{1, 2, 3, 4, 5}
 	var grid [3][4]int
 
 	fmt.Println(arr1)
@@ -17,6 +31,14 @@ func main() {
 	for i := 0; i < len(arr3); i++ {
 		fmt.Println(arr3[i])
 	}
+
+	printArr(arr3)
+
+	for i, v := range arr3 {
+		fmt.Println(i, v)
+	}
+
+	printArr2(&arr3)
 
 	for i, v := range arr3 {
 		fmt.Println(i, v)
