4f3c13122e7c83b2563e2355093e86462b99a0d8
翁某小小 <343577402@qq.com>
2019-03-18 15:29:24 +0800

    广度优先算法

---
+++ src/maze/maze.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	maze := readMaze("src/maze/maze.in")
+	steps := walk(maze, point{0, 0}, point{len(maze) - 1, len(maze[0]) - 1})
+
+	for _, row := range steps {
+		for _, val := range row {
+			fmt.Printf("%3d ", val)
+		}
+		fmt.Println()
+	}
+}
+
+type point struct {
+	i, j int
+}
+
+var dirs = [4]point{
+	{-1, 0}, {0, -1}, {1, 0}, {0, 1},
+}
+
+/**
+ 两个节点相加
+ */
+func (p point) add(r point) point {
+	return point{p.i + r.i, p.j + r.j}
+}
+/**
+	判断 节点是否在数组中
+ */
+func (p point) at(grid [][]int) (int, bool) {
+
+	if p.i < 0 || p.i >= len(grid) {
+		return 0, false
+	}
+	if p.j < 0 || p.j >= len(grid[p.i]) {
+		return 0, false
+	}
+
+	return grid[p.i][p.j], true
+}
+
+/**
+	走迷宫
+ */
+func walk(maze [][]int, start, end point) [][]int {
+	steps := make([][]int, len(maze))
+	for i := range maze {
+		steps[i] = make([]int, len(maze[i]))
+	}
+
+	Q := []point{start}
+	for len(Q) > 0 {
+		cur := Q[0]
+		Q = Q[1:]
+
+		if cur == end {
+			break
+		}
+
+		for _, dir := range dirs {
+			next := cur.add(dir)
+
+			val, ok := next.at(maze)
+			if !ok || val == 1 {
+				continue
+			}
+
+			val, ok = next.at(steps)
+			if !ok || val != 0 {
+				continue
+			}
+
+			if next == start {
+				continue
+			}
+
+			curSteps, _ := cur.at(steps)
+			steps[next.i][next.j] = curSteps + 1
+
+			Q = append(Q, next)
+		}
+	}
+
+	return steps
+}
+
+/**
+ 	从文件中读取数组
+ */
+func readMaze(filename string) [][]int {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	var row, col int
+	_, err = fmt.Fscanf(file, "%d %d", &row, &col)
+	if err != nil {
+		panic(err)
+	}
+
+	maze := make([][]int, row)
+
+	for i := range maze {
+		maze[i] = make([]int, col)
+		for j := range maze[i] {
+			_, err := fmt.Fscanf(file, "%d", &maze[i][j])
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+
+	return maze
+}
